371ca967ae0cd93a92edeadcd2142d6f341820ac
kny-i <kny_i@keio.jp>
2022-07-26 21:05:58 +0900

    add main

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,24 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact {
+
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string nickName;
+	std::string darkestSecret;
+
+public:
+	Contact();
+	Contact(std::string firstName, std::string lastName, std::string nickName, std::string darkestSecret);
+	~Contact();
+	const std::string &getFirstName() const;
+	const std::string &getLastName() const;
+	const std::string &getNickName() const;
+};
+
+#endif
--- cpp00/ex01/main.c
+++ cpp00/ex01/main.c
@@ -1,7 +1,32 @@
-#include "Contact.hpp
 #include "PhoneBook.hpp"
 
-int main ()
-{
+int main(void) {
+	PhoneBook phoneBook;
+	int instructionNumber;
 
+	while (1) {
+		std::cout << std::endl << "****************[menu]****************" << std::endl;
+		std::cout << "input instruction (1.ADD, 2.SEARCH, 3.EXIT, (4.VIEW)) > ";
+		std::cin >> instructionNumber;
+
+		switch (instructionNumber) {
+			case 1 : {
+				phoneBook.executeAdd();
+				break;
+			}
+			case 2 : {
+				phoneBook.executeSearch();
+				break;
+			}
+			case 3 :
+				exit(0);
+			case 4 : {
+				phoneBook.executeShowList();
+				break;
+			}
+			default : {
+				std::cout << "invalid input!" << std::endl;
+			}
+		}
+	}
 }
