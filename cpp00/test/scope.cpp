e308b149206ff80336dee12c28db3bd8009d1a56
kny-i <kny_i@keio.jp>
2022-07-26 21:06:24 +0900

    test

---
+++ cpp00/test/scope.cpp
@@ -0,0 +1,43 @@
+/*
+#include <iostream>
+using namespace std;
+
+class Kitty {
+public:
+	char *str;
+	void print(char *);
+};
+
+void Kitty::print(char *str) {
+	cout << str;
+}
+
+int main() {
+	Kitty obj;
+	obj.str = "Kitty on your lap";
+	obj.print("Di_Gi_Gharat");
+	return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+class Kitty {
+public:
+	char *str;
+	void print(char *);
+};
+
+void Kitty::print(char *str) {
+	cout << str << '\n';
+	cout << Kitty::str << '\n';
+}
+
+int main() {
+	Kitty obj;
+	obj.str = "Kitty on your lap";
+
+	obj.print("Di_Gi_Gharat");
+	return 0;
+}
