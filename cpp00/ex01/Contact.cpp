652e04cb7b9a83ee6c26b6c7e15ea04e8ae8fd7b
kny-i <kny_i@keio.jp>
2022-08-10 21:16:11 +0900

    modify files

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -8,3 +8,26 @@ Contact::Contact()
 		  darkest_secret(""),
 		  isFilled(false) {}
 
+void 			Contact::setFirstName(const std::string str) { first_name = str; }
+
+void 			Contact::setLastName(const std::string str) { last_name = str; }
+
+void 			Contact::setNickname(const std::string str) { nick_name = str; }
+
+void 			Contact::setPhoneNumber(const std::string str) { phone_number = str; }
+
+void 			Contact::setDarkestSecret(const std::string str) { darkest_secret = str; }
+
+std::string 	Contact::getFirstName() const { return first_name; }
+
+std::string 	Contact::getLastName() const { return last_name; }
+
+std::string 	Contact::getNickname() const { return nick_name; }
+
+std::string 	Contact::getPhoneNumber() const { return phone_number; }
+
+std::string 	Contact::getDarkestSecret() const { return darkest_secret; }
+
+int 			Contact::checkDone() const { return isFilled; }
+
+void 			Contact::done() { isFilled = true; }
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -20,18 +20,18 @@ public:
 
 	Contact();
 
-	void 			setFirstName(const std::string str) { first_name = str; }
-	void 			setLastName(const std::string str) { last_name = str; }
-	void 			setNickname(const std::string str) { nick_name = str; }
-	void 			setPhoneNumber(const std::string str) { phone_number = str; }
-	void 			setDarkestSecret(const std::string str) { darkest_secret = str; }
-	std::string 	getFirstName() const { return first_name; }
-	std::string 	getLastName() const { return last_name; }
-	std::string 	getNickname() const { return nick_name; }
-	std::string 	getPhoneNumber() const { return phone_number; }
-	std::string 	getDarkestSecret() const { return darkest_secret; }
-	int 			checkDone() const { return isFilled; }
-	void 			done() { isFilled = true; }
+	void 			setFirstName(const std::string str);
+	void 			setLastName(const std::string str);
+	void 			setNickname(const std::string str);
+	void 			setPhoneNumber(const std::string str);
+	void 			setDarkestSecret(const std::string str);
+	std::string 	getFirstName()const;
+	std::string 	getLastName()const;
+	std::string 	getNickname()const;
+	std::string 	getPhoneNumber()const;
+	std::string 	getDarkestSecret()const;
+	int 			checkDone()const;
+	void 			done();
 };
 
 #endif
