87ef240980f143ec03c2a22690844a392acda1ff
kny-i <kny_i@keio.jp>
2022-08-12 17:27:15 +0900

    add files

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,21 @@
+#include "Harl.hpp"
+
+void test(std::string level)
+{
+	Harl Harl;
+
+
+	std::cout << level << std::endl;
+	Harl.complain(level);
+	std::cout << std::endl;
+}
+
+int main()
+{
+	test("DEBUG");
+	test("INFO");
+	test("WARNING");
+	test("ERROR");
+	test("B");
+	test("");
+}
