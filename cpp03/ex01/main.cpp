8cf03e2c3076fe7cba72e8dd8ae7e095c58e08b3
kny-i <kny_i@keio.jp>
2022-08-24 19:47:20 +0900

    modify main

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -2,12 +2,32 @@
 
 int	main()
 {
+	std::cout << "----clap trap test----" << std::endl;
+	ClapTrap fox("fox");
+	fox.printInfo();
+	fox.attack("cat");
+	fox.takeDamage(2);
+	fox.beRepaired(3);
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.attack("cat");
+	fox.printInfo();
+	fox.takeDamage(100);
+	fox.printInfo();
 	ScavTrap dog("dog");
 	dog.printInfo();
 	dog.attack("cat");
 	dog.takeDamage(2);
 	dog.beRepaired(3);
 	dog.attack("cat");
+
+	std::cout << "----scav trap test----" << std::endl;
 /*		for (int i = 0; i < 100; i++)
 	{
 		dog.attack("cat");
