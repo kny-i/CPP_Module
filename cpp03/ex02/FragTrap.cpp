4efea5393f849ec0c5f4d9d7ec56816fb9594a57
kny-i <kny_i@keio.jp>
2022-08-25 02:18:30 +0900

    modify fd

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -7,12 +7,12 @@ FragTrap::FragTrap(): ClapTrap()
 	this->hitPoint = 100;
 	this->energyPoint = 100;
 	this->attackDamage = 30;
-	std::cout << "FragTrap Default Constructor called" << std::endl;
+	std::cerr << "FragTrap Default Constructor called" << std::endl;
 }
 
 FragTrap::FragTrap(const FragTrap &copy): ClapTrap(copy)
 {
-	std::cout << "FragTrap Copy Constructor called" << std::endl;
+	std::cerr << "FragTrap Copy Constructor called" << std::endl;
 }
 
 FragTrap::FragTrap(std::string str): ClapTrap(str)
@@ -20,21 +20,24 @@ FragTrap::FragTrap(std::string str): ClapTrap(str)
 	this->hitPoint = 100;
 	this->energyPoint = 100;
 	this->attackDamage = 30;
-	std::cout << "FragTrap Constructor for the name " << this->name << " called" << std::endl;
+	std::cerr << "FragTrap Constructor for the name " << this->name << " called" << std::endl;
 }
 
 FragTrap::~FragTrap()
 {
-	std::cout << "FragTrap Deconstructor for " << this->name << " called" << std::endl;
+	std::cerr << "FragTrap Deconstructor for " << this->name << " called" << std::endl;
 }
 
 FragTrap &FragTrap::operator=(const FragTrap &other)
 {
-	std::cout << "FragTrap Assignation operator called" << std::endl;
-	this->name = other.name;
-	this->hitPoint = other.hitPoint;
-	this->energyPoint = other.energyPoint;
-	this->attackDamage = other.attackDamage;
+	if (this != &other)
+	{
+		std::cout << "FragTrap Assignation operator called" << std::endl;
+		this->name = other.name;
+		this->hitPoint = other.hitPoint;
+		this->energyPoint = other.energyPoint;
+		this->attackDamage = other.attackDamage;
+	}
 	return *this;
 }
 
